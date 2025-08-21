861e8e9647328694ded0af06a4927a2848a6187d
Hu Bowen <contact@hubowen.dev>
2025-08-21 23:01:51 +0800

    fix definitions

--- template.cpp
+++ template.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-#define ll long long;
-#define pii pair<int, int>;
-#define pll pair<long, long>;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long, long>
 #define vi vector<int>
 #define vll vector<long long>
 
