502c73c97395d0a6ca8eb9e541c53b8976ee03fa
Hu Bowen <85286288+notbowen@users.noreply.github.com>
2026-01-29 23:23:16 +0800

    add solve fn

--- template.cpp
+++ template.cpp
@@ -11,12 +11,18 @@ using namespace std;
 #define pb push_back
 #define eb emplace_back
 
+void solve() {
+    
+}
+
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
 
-
+  int t;
+  cin >> t;
+  while (t--) solve();
 
   return 0;
 }
